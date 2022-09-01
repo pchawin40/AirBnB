f9e1b570bbf631bbfe2dd3793fd89b0ec9cc434e
chawin <pathocha000@gmail.com>
2022-08-31 22:32:12 -0700

    Revised some

--- frontend/src/components/LandingPage/HomeFooterBar/HomeFooterBar.js
+++ frontend/src/components/LandingPage/HomeFooterBar/HomeFooterBar.js
@@ -18,12 +18,12 @@ const HomeFooterBar = () => {
   const [showMapModal, setShowMapModal] = useState(false);
 
   return (
-    <div className="outer-footer-container">
-      <div className="footer-content-container">
+    <div className="outer-footer-container_home">
+      <div className="footer-content-container_home">
         {/* //? upper div container (map) */}
-        <div id="DIV_MAP">
-          <button id="inner_div_map_button" onClick={_ => setShowMapModal(true)}>
-            Show map
+        <div id="DIV_MAP_home">
+          <button id="inner_div_map_button_home" onClick={_ => setShowMapModal(true)}>
+            <span>Show map</span>
             <i className="fa-solid fa-map"></i>
           </button>
         </div>
@@ -37,48 +37,48 @@ const HomeFooterBar = () => {
         }
 
         {/* //? lower div container (lower) */}
-        <div className="lower_div_container">
+        <div className="lower_div_container_home">
           
           {/* //? outer lower div 1 */}
-          <div id="DIV_1">
-            <span className="inner_div_1_content" id="SPAN_2">© 2022 Airbnb, Inc.</span>
-            <span className="inner_div_1_content" id="SPAN_3">·</span>
-            <span className="inner_div_1_content" id="A_4">Privacy</span>
-            <span className="inner_div_1_content" id="SPAN_5">·</span>
-            <span className="inner_div_1_content" id="A_6">Terms</span>
-            <span className="inner_div_1_content" id="SPAN_7">·</span>
-            <span className="inner_div_1_content" id="A_8">Sitemap</span>
+          <div id="DIV_1_home">
+            <span className="inner_div_1_content_home" id="SPAN_2">© 2022 Airbnb, Inc.</span>
+            <span className="inner_div_1_content_home" id="SPAN_3">·</span>
+            <span className="inner_div_1_content_home" id="A_4">Privacy</span>
+            <span className="inner_div_1_content_home" id="SPAN_5">·</span>
+            <span className="inner_div_1_content_home" id="A_6">Terms</span>
+            <span className="inner_div_1_content_home" id="SPAN_7">·</span>
+            <span className="inner_div_1_content_home" id="A_8">Sitemap</span>
           </div>
 
           {/* //? outer lower div 2 */}
-          <div id="DIV_2">
-            <div id="inner_div_1">
-              <span className="inner_div_1_content">
+          <div id="DIV_2_home">
+            <div id="inner_div_1_home">
+              <span className="inner_div_1_content_home">
                 {/* globe icon */}
-                <i className="fa-solid fa-globe" id="inner_div_1_globe"></i>
+                <i className="fa-solid fa-globe" id="inner_div_1_globe_home"></i>
               </span>
-              <span className="inner_div_1_content">
+              <span className="inner_div_1_content_home">
                 {/* Language */}
                 English (US)
               </span>
             </div>
 
-            <div id="inner_div_2">
-              <span className="inner_div_2_content_a">
+            <div id="inner_div_2_home">
+              <span className="inner_div_2_content_a_home">
                 {/* currency */}
                 $
               </span>
-              <span className="inner_div_2_content_b">
+              <span className="inner_div_2_content_b_home">
                 {/* dollar sign */}
                 USD
               </span>
             </div>
 
             {/* //? outer lower div 3 */}
-            <div id="inner_div_3">
+            <div id="inner_div_3_home">
               {/* Support & resources */}
-              <span className="inner_div_3_content">Support & resources</span>
-              <i className="fa-solid fa-angle-up"></i>
+              <span className="inner_div_3_content_home">Support & resources</span>
+              <i className="fa-solid fa-angle-up up_arrow_home"></i>
             </div>
           </div>
         </div>
--- frontend/src/components/LandingPage/UserLoginRegistration/SignupFormPage/SignUpFormPage.js
+++ frontend/src/components/LandingPage/UserLoginRegistration/SignupFormPage/SignUpFormPage.js
@@ -1,11 +1,17 @@
 // import css
 import './SignUpFormPage.css';
 
+// import react
+import { useState, useRef } from 'react';
+
 // import react-redux
-import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 // import react-router-dom
-import { useHistory } from 'react-router-dom';
+import { useHistory, Redirect } from 'react-router-dom';
+
+// import store
+import * as sessionActions from '../../../../store/session';
 
 //? SignupFormPage component
 const SignupFormPage = () => {
@@ -15,10 +21,102 @@ const SignupFormPage = () => {
   // invoke useRef
   const ref = useRef(null);
 
+  /**
+   * Controlled Inputs:
+   * ------------------
+   * firstName: new user's first name
+   * lastName: new user's last name
+   * email: new user's email
+   * password: new user's password
+   * confirmedPassword: new user's confirmed password
+   * image: new user's image
+   * images: new user's images
+   * validationErrors: new user's validation errors
+   */
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmedPassword, setConfirmedPassword] = useState('');
+  const [image, setImage] = useState(null);
+
+  // for multiple image file upload
+  // const [images, setImages] = useState(null);
+  const [validationErrors, setValidationErrors] = useState([]);
+
+  // invoke useDispatch
+  const dispatch = useDispatch();
+
   //? handleScrollClick
   const handleScrollClick = () => {
     ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
+
+  // get session user
+  const sessionUser = useSelector(sessionActions.getSessionUser);
+
+  //? handleSubmit: handle form submit
+
+  // if session user exist, return to '/' path
+  if (sessionUser) return <Redirect to='/' />;
+
+  const handleSubmit = e => {
+    // prevent page from refreshing
+    e.preventDefault();
+
+    // validation that confirm password is same as password fields
+    if (confirmedPassword !== password) {
+      // else if password does not match
+      return setValidationErrors("Password does not match")
+    }
+
+    // reset validation errors
+    setValidationErrors([]);
+
+    // user to sign up
+    const user = {
+      firstName,
+      lastName,
+      email,
+      password,
+      image
+    };
+
+    // reset form data to default value after signing up
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setConfirmedPassword("");
+    setImage(ref);
+
+    // reset ref value
+    ref.current.value = "";
+
+    // dispatch signup thunk action
+    // handle and display errors if any
+    dispatch(sessionActions.signup(user)).catch(
+      async res => {
+        // parse error data
+        const data = await res.json();
+
+        // set any error data into validation errors
+        if (data.errors) setValidationErrors(Object.values(data.errors));
+      }
+    );
+  };
+
+  //? updateFile: single file update function
+  const updateFile = e => {
+    const file = e.target.files[0];
+    if (file) setImage(file);
+  }
+
+  //? updateFiles: multiple file update function
+  // const updateFiles = e => {
+  //   const files = e.target.files;
+  //   setImages(files);
+  // }
 
   return (
     <section className="sign-up-form-page-container">
@@ -79,44 +177,124 @@ const SignupFormPage = () => {
         <section className="main-pic-carousel">
           <section className="inner-main-pic-carousel">
 
-              {/* image 1 */}
-              <img src='https://i.picsum.photos/id/1018/3914/2935.jpg?hmac=3N43cQcvTE8NItexePvXvYBrAoGbRssNMpuvuWlwMKg' />
+            {/* image 1 */}
+            <img src='https://i.picsum.photos/id/1018/3914/2935.jpg?hmac=3N43cQcvTE8NItexePvXvYBrAoGbRssNMpuvuWlwMKg' />
 
+            {/* image 2 */}
+            <img src='https://i.picsum.photos/id/101/2621/1747.jpg?hmac=cu15YGotS0gIYdBbR1he5NtBLZAAY6aIY5AbORRAngs' />
 
-              {/* image 2 */}
-              <img src='https://i.picsum.photos/id/101/2621/1747.jpg?hmac=cu15YGotS0gIYdBbR1he5NtBLZAAY6aIY5AbORRAngs' />
-
-
-
-              {/* image 3 */}
-              <img src={`https://i.picsum.photos/id/1008/5616/3744.jpg?hmac=906z84ml4jhqPMsm4ObF9aZhCRC-t2S_Sy0RLvYWZwY`} />
-
+            {/* image 3 */}
+            <img src={`https://i.picsum.photos/id/1041/5184/2916.jpg?hmac=TW_9o6HeD7H7I7NVo-S1Fa1iAvzQ10uvmJqsXvNoi0M`} />
 
           </section>
-
-          <section>
-            {/* article */}
-
-          </section>
-        </section>
-
-        {/* user's quote */}
-        <section>
-
         </section>
       </main>
 
       {/* //? Sign Up Form */}
-      <section className="sign-up-form">
-        <form>
+      <section className="sign-up-form-container">
+        <section className="sign-up-form-left">
+          <form className="sign-up-form" onSubmit={handleSubmit}>
 
-        </form>
+            <h2>
+              Welcome to Airbnb
+            </h2>
+
+            <ul>
+              {
+                validationErrors.map(error => <li key={error}>{error}</li>)
+              }
+            </ul>
+
+            {/* //? firstName */}
+            <input
+              placeholder="First name"
+              type='firstName'
+              onChange={e => setFirstName(e.target.value)}
+              required
+              value={firstName}
+            >
+            </input>
+
+            {/* //? lastName */}
+            <input
+              placeholder="Last name"
+              type='lastName'
+              onChange={e => setLastName(e.target.value)}
+              required
+              value={lastName}
+            >
+            </input>
+
+            {/* //? email */}
+            <input
+              placeholder="Email"
+              type='email'
+              onChange={e => setEmail(e.target.value)}
+              required
+              value={email}
+            >
+            </input>
+
+            {/* //? password */}
+            <input
+              placeholder="Password"
+              type='password'
+              onChange={e => setPassword(e.target.value)}
+              required
+              value={password}
+            >
+            </input>
+
+            {/* //? confirmedPassword */}
+            <input
+              placeholder="Confirm"
+              type='password'
+              onChange={e => setConfirmedPassword(e.target.value)}
+              required
+              value={confirmedPassword}
+            >
+            </input>
+
+            {/* //? Single File Upload */}
+            {/* //* putting ref clears file input upon invalid submission */}
+            <div className="form-image-upload-container">
+              <span>Your profile picture:</span>
+              <input id="form-image-upload" type="file" onChange={updateFile} ref={ref} />
+            </div>
+
+            {/* //? Multiple File Upload */}
+            {/* <label>
+            Multiple Upload
+            <input
+              type="file"
+              multiple
+              onChange={updateFiles} />
+          </label> */}
+
+            {/* //? Sign Up Button*/}
+            <button type="submit">
+              Sign Up
+            </button>
+          </form>
+
+          <div>
+            {sessionUser && (
+              <div>
+                <h1>{sessionUser.username}</h1>
+                <img
+                  style={{ width: "150px" }}
+                  src={sessionUser.profileImageUrl}
+                  alt="profile"
+                />
+              </div>
+            )}
+          </div>
+
+        </section>
+        <section className="sign-up-form-right">
+          <img src="https://a0.muscache.com/im/pictures/f409b291-8b55-4780-81c3-a067062982d1.jpg?im_w=2560&im_q=highq" />
+        </section>
       </section>
-
-      {/* //? Footer */}
-      <footer className="sign-up-footer">
-        {/* //? Spot Footer */}
-      </footer>
     </section>
   );
 };
